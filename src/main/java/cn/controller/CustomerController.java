d5f677060384eab9ce33d2d5e3de212d51648bb0
test1 <597326198@qq.com>
2019-11-26 12:58:13 +0800

    Initial commit

---
+++ src/main/java/cn/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package cn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController{
+
+        @RequestMapping("/list")
+        public String list( ) throws Exception {
+            return "customer";
+        }
+
+}
